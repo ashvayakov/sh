3208501b9318321abfaa0bd04a2ec4a37de7f93b
Aliaksandr Shvayakou <a.shvayakov@gmail.com>
2022-02-05 15:43:44 -0500

    Update 5600_get_domain_capacity.sh

--- 5600_get_domain_capacity.sh
+++ 5600_get_domain_capacity.sh
@@ -3,7 +3,7 @@
 # The first argument is Domain ID, use 0 by default
 domID=${1:-0}
  
-ssh -p22 shvayakov@10.2.1.90 <<EOF
+ssh -p22 sadmin10.2.1.90 <<EOF
 # Print RAW capacity for selected disk domain
 show disk_domain general |filterRow column=ID predict=equal_to value=$domID |filterColumn include columnList=Free\sCapacity
  
