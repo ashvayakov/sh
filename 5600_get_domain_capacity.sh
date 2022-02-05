bbab86975aaf8655649d9672712dce5b0554c7de
Aliaksandr Shvayakou <a.shvayakov@gmail.com>
2022-02-05 15:43:09 -0500

    Add files via upload

---
+++ 5600_get_domain_capacity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+# The first argument is Domain ID, use 0 by default
+domID=${1:-0}
+ 
+ssh -p22 shvayakov@10.2.1.90 <<EOF
+# Print RAW capacity for selected disk domain
+show disk_domain general |filterRow column=ID predict=equal_to value=$domID |filterColumn include columnList=Free\sCapacity
+ 
+# Print Available capacity for selected disk domain
+show disk_domain available_capacity disk_domain_id=$domID raid_level=RAID5-8|filterColumn include columnList=Tier2\sAvailable\sCapacity
+EOF
+
